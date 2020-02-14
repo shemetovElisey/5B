a9f59f1954fc1d194f74136f1936b1d002d2928c
Elisey <shemetov.elisey@gmail.com>
2020-02-14 18:03:49 +0700

    Initial Commit

---
+++ 5B/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  5B
+//
+//  Created by Елисей on 14.02.2020.
+//  Copyright © 2020 Longjects. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
