fa11a68dfa200f7c41719a3c192b3d5d9dae9303
Gudov <dima@novator.su>
2020-02-15 17:35:03 +0700

    add list input and debug print

--- 5B/main.cpp
+++ 5B/main.cpp
@@ -7,17 +7,44 @@
 //
 
 #include <iostream>
+#include <stdio.h>
 
 struct list {
 	int v;
 	list *p, *n;
 
-	list( int value = 0, list* prev = NULL, list* next = NULL ) {
-
-	}
+	list( int value, list *prev, list *next ) 
+		: v(value), p(prev), n(next)
+	{}
 };
 
+list *input() {
+	int in;
+	scanf( "%d", &in );
+	list *p, *b = new list( in, NULL, NULL );
+	p = b;
+
+	scanf( "%d", &in );
+	while( in > 0 ) {
+		b = new list( in, b, NULL );
+		b->p->n = b;
+
+		scanf( "%d", &in );
+	}
+
+	return p;
+}
+
+void print_list(list *l) {
+	while(l) {
+		printf( "%d ", l->v );
+		l = l->n;
+	}
+	printf("\n");
+}
+
 int main() {
-    
+    list* l = input();
+	print_list(l);
     return 0;
 }
