557d0b6cee37c02cb8c5577fb9e92ffa32d3ca49
Elisey <shemetov.elisey@gmail.com>
2020-02-16 15:31:21 +0700

    Update main.cpp
    fix some mistakes

--- 5B/main.cpp
+++ 5B/main.cpp
@@ -6,9 +6,87 @@
 //  Copyright © 2020 Longjects. All rights reserved.
 //
 
-#include <iostream>
 #include <stdio.h>
+#include <locale.h>
 
-int main() {
+#define printf_s printf
+#define scanf_s scanf
+
+struct list
+{
+    int value;
+    list *prev, *next;
+
+    list(int value, list *prev, list *next)
+        : value(value), prev(prev), next(next)
+    {}
+};
+
+list *input()
+{
+    int numberOfElements;
+    int inputVar;
+    
+    printf_s("Введите количество элементов: ");
+    scanf_s("%d", &numberOfElements);
+    
+    printf_s("Введите действительные числа: \n");
+    scanf_s("%d", &inputVar);
+    
+    list *p, *b = new list(inputVar, NULL, NULL);
+    p = b;
+    
+    while(numberOfElements > 1)
+    {
+        scanf_s("%d", &inputVar);
+        b = new list(inputVar, b, NULL);
+        b->prev->next = b;
+        numberOfElements--;
+    }
+
+    return p;
+}
+
+list* list_end(list* l)
+{
+    while(l->next != NULL){ l = l->next; }
+    return l;
+}
+
+int list_mult(list *expr)
+{
+    list *end = list_end(expr);
+
+    int mult = 1;
+    while(expr->next)
+    {
+        mult = mult * (expr->value + expr->next->value + 2 * end->value);
+        expr = expr->next;
+        end = end->prev;
+    }
+
+    return mult;
+}
+
+void print_list(list *expr)
+{
+    printf_s("Элементы списка: ");
+    // вывод элементов списка
+    while(expr)
+    {
+        printf_s("%d ", expr->value);
+        expr = expr->next;
+    }
+    
+    printf_s("\n");
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    list* expression = input();                 // ввод значений
+    print_list(expression);                     // печать значений
+    printf_s("%d\n", list_mult(expression));    // вывод ответа
+    
     return 0;
 }
